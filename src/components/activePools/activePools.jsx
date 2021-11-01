64e240c01394c8a580cab634aba11c2a66da6ab4
Salil Naik <salil.naik27@gmail.com>
2021-11-02 02:20:45 +0530

    add: other pools component & pool data

---
+++ src/components/activePools/activePools.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import style from "./active-pools.module.scss";
+
+export const ActivePools = ({ pools, name }) => {
+  const [tokens, setTokens] = useState([]);
+
+  //   if the number of pools exceed 4, show 3 and hide the rest.
+  useEffect(() => {
+    let len = pools.length;
+    if (len > 4) {
+      let tknArr = pools.slice(0, 3);
+      setTokens(tknArr);
+    } else {
+      setTokens(pools);
+    }
+  }, [pools]);
+
+  //   checks if the image exists in the databse.
+  let fetchImage = (name) => {
+    try {
+      return require(`../../assets/avatar/${formattedName(name)}.png`).default;
+    } catch (err) {
+      return false;
+    }
+  };
+
+  const formattedName = (name) => {
+    return name.split(" ").join("-").toLowerCase();
+  };
+
+  return (
+    <div className={style.container}>
+      {tokens.map((item, index) => {
+        //   if image url is broken, show 1st character of the token.
+        if (fetchImage(name)) {
+          return <img src={fetchImage(name)} alt="avatar" key={index} className={style.token} />;
+        } else {
+          return (
+            <div key={`${index}-no-img`} className={style.token}>
+              {item.token[0]}
+            </div>
+          );
+        }
+      })}
+      {/* show the number of hidden tokens */}
+      {pools.length > 4 && (
+        <div className={`${style.token} ${style.count}`}>
+          +{pools.length - 3}
+        </div>
+      )}
+    </div>
+  );
+};
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,5 +1,7 @@
 import style from "./list-item.module.scss";
 
+import { ActivePools } from "../activePools/index";
+
 export const ListItem = ({ pool }) => {
   const {
     id,
@@ -12,28 +14,32 @@ export const ListItem = ({ pool }) => {
     collateral,
     progress,
     status,
+    activePools,
   } = pool;
 
+  //   calculates the tokens already collected (as per the % provided)
   let collectedTokens = () => {
     return (progress * borrowing) / 100;
   };
 
+  //   returns false if the image is unavailable or url is broken
   let fetchImage = (name) => {
     try {
-        return require(`../../assets/avatar/${formattedName(name)}.png`).default;
+      return require(`../../assets/avatar/${formattedName(name)}.png`).default;
     } catch (err) {
       return false;
     }
   };
 
   const formattedName = (name) => {
-    return name.split(' ').join('-').toLowerCase();
-  }
+    return name.split(" ").join("-").toLowerCase();
+  };
 
   return (
     <div className={style.list}>
       <div className="col-3">
         <div className="d-flex align-items-center">
+          {/* show the first character of the name if avatar isn't available */}
           {fetchImage(name) ? (
             <img
               src={fetchImage(name)}
@@ -41,14 +47,20 @@ export const ListItem = ({ pool }) => {
               className={style["list__avatar"]}
             />
           ) : (
-            <div className={style["list__avatar"]}>{name?name[0]:'A'}</div>
+            <div className={style["list__avatar"]}>{name ? name[0] : "A"}</div>
           )}
 
           <div className={style["list__details"]}>
-            <p className={style.accountName}>{name ? name: `Anonymous#${id}`}</p>
+            <p className={style.accountName}>
+              {name ? name : `Anonymous#${id}`}
+            </p>
             <div className={style["tags-container"]}>
               {tags.map((tag) => {
-                return <span className={style.tag} key={tag}>{tag}</span>;
+                return (
+                  <span className={style.tag} key={tag}>
+                    {tag}
+                  </span>
+                );
               })}
             </div>
           </div>
@@ -60,12 +72,14 @@ export const ListItem = ({ pool }) => {
           {borrowing} <span className={style.token}>{token}</span>
         </Param>
         <Param>{collateral}%</Param>
-        <Param className={style["pool-param"]}>
+        <Param>
           {progress}% ({collectedTokens(progress)}{" "}
           <span className={style.token}>{token}</span>)
         </Param>
       </div>
       <div className="col-3 d-flex justify-content-end">
+        {/* other active pools by the same user */}
+        {activePools && <ActivePools pools={activePools} name={name} />}
         <a href="#salil" className="btn btn-light">
           Lend
         </a>
