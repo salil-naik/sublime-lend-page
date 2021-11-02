c641a0bb3a1beaef6950f476e8557a9454ce4691
Salil Naik <salil.naik27@gmail.com>
2021-11-02 15:51:56 +0530

    verify btn, icons to stats

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,15 +4,28 @@ import style from "./header.module.scss";
 import { NavLink } from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <header className={style.header}>
-            <img src={logo} alt="sublime logo" className={style.logo}/>
-            <nav className={style.nav}>
-                <NavLink to="/dashboard" activeClassName={style.active}>Dashboard</NavLink>
-                <NavLink to="/borrow" activeClassName={style.active}>Borrow</NavLink>
-                <NavLink to="/lend" activeClassName={style.active}>Lend</NavLink>
-                <NavLink to="/profile" activeClassName={style.active}>Your profile</NavLink>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className={style.header}>
+      <div className="d-flex align-items-center">
+        <img src={logo} alt="sublime logo" className={style.logo} />
+        <nav className={style.nav}>
+          <NavLink to="/dashboard" activeClassName={style.active}>
+            Dashboard
+          </NavLink>
+          <NavLink to="/borrow" activeClassName={style.active}>
+            Borrow
+          </NavLink>
+          <NavLink to="/lend" activeClassName={style.active}>
+            Lend
+          </NavLink>
+          <NavLink to="/profile" activeClassName={style.active}>
+            Your profile
+          </NavLink>
+        </nav>
+      </div>
+      <button className="btn btn-light" style={{ justifySelf: "flex-end" }}>
+        Get verified
+      </button>
+    </header>
+  );
+};
--- src/components/statsCard/statsCard.jsx
+++ src/components/statsCard/statsCard.jsx
@@ -1,13 +1,20 @@
 import style from "./stats.module.scss";
+import { ReactComponent as PercentageIcon } from "../../assets/icons/percentage.svg";
+import { ReactComponent as CalendarIcon } from "../../assets/icons/calendar.svg";
+import { ReactComponent as DollarIcon } from "../../assets/icons/dollar.svg";
 
-export const StatsCard = ({item}) => {
-    return (
-        <div className={style.card}>
-            <div className={style['card__icon']}></div>
-            <div className={style['card__details']}>
-                <p className={style.value}>{item.value}</p>
-                <p className={style.desc}>{item.desc}</p>
-            </div>
-        </div>
-    )
-}
+export const StatsCard = ({ item }) => {
+  return (
+    <div className={style.card}>
+      <div className={`${style["card__icon"]} ${style[item.id]}`}>
+        {item.id === "return-rate" && <PercentageIcon />}
+        {item.id === "amount-lent" && <DollarIcon />}
+        {item.id === "roi" && <CalendarIcon />}
+      </div>
+      <div className={style["card__details"]}>
+        <p className={style.value}>{item.value}</p>
+        <p className={style.desc}>{item.desc}</p>
+      </div>
+    </div>
+  );
+};
